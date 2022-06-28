0aca9ff49af6e2a5c9cf30e4637c278778ba5825
estebancolorado <esteban.colorado@ceiba.com.co>
2022-06-28 10:32:33 -0500

    Refactoring 1.2

--- microservicio/infraestructura/src/main/java/com/ceiba/reservation/controller/ReservationControllerCommand.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/reservation/controller/ReservationControllerCommand.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/reservations")
 @Tag(name = "Reservation Controller Command")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ReservationControllerCommand
 {
     private final SaveReservationHandler saveReservationHandler;
